2395fc83df2951201fcdcca3f0dd2987a9aeed66
jeeva <jeevadhasan1996@gmail.com>
2026-06-27 23:42:30 +0530

    add: variable exercise

---
+++ Week-01-JavaScript-Basics/variable.js
@@ -0,0 +1,49 @@
+let message; // variable declaration, let can be re-assigned    
+
+
+let user = 'Jeeva', age = 25, message = 'Hello'; // also declare but this is not the best way
+
+message = 'Hello!';
+
+message = 'World!'; // value changed
+
+alert(message);
+
+
+const userName = 'Jeeva'; // constant variable, cannot be re-assigned
+
+userName = 'John'; // This will throw an error because userName is a constant
+
+
+let admin;
+let name;
+name = "jeeva";
+admin = name;
+alert(admin); // This will display "jeeva" in an alert box
+
+// Giving the right name to the variable
+
+const ourPlanetName = "Earth";
+const current_visitor = "Jeeva";
+
+
+
+// Things need to know
+
+
+// we can decalare a variable using this dollay and underscore sign, but it is not recommended to use them as variable names.
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+
+alert($ + _); // 3
+
+// incorrect variable names:
+
+// let 1a; // cannot start with a digit
+
+// let my-name; // hyphens '-' aren't allowed in the name
+
+
+
+
+
